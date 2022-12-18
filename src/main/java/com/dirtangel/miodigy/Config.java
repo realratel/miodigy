aab5e0a1d1eef59f6459caeea1e67b525e54ca57
mmiod <mmioduszewski87@gmail.com>
2022-12-18 22:39:56 +0100

    frontend part1

---
+++ src/main/java/com/dirtangel/miodigy/Config.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dirtangel.miodigy;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author mmiod
+ */
+@Configuration
+public class Config {
+    
+    
+//    @Bean(destroyMethod = "")
+//@ConditionalOnMissingBean(CamelContext.class)
+//CamelContext camelContext(ApplicationContext applicationContext,
+//             CamelConfigurationProperties config) throws Exception {
+//  CamelContext camelContext = new SpringCamelContext(applicationContext);
+//  return doConfigureCamelContext(applicationContext, camelContext, config);
+}
