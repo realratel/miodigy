6e2731d6be9987bd8a9379c63d09debbde9b5b75
Miodigy <miodigy@gmail.com>
2022-12-26 23:27:46 +0100

    template resolver

--- src/main/java/com/dirtangel/miodigy/Config.java
+++ src/main/java/com/dirtangel/miodigy/Config.java
@@ -4,18 +4,85 @@
  */
 package com.dirtangel.miodigy;
 
+import java.util.Collections;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafView;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 /**
  *
  * @author mmiod
  */
 @Configuration
-public class Config {
+@EnableWebMvc
+@ComponentScan
+public class Config extends WebMvcConfigurerAdapter implements WebMvcConfigurer, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public Config() {
+        super();
+    }
+
+    @Bean
+    @Description("Thymeleaf template resolver serving HTML 5")
+    public ClassLoaderTemplateResolver templateResolver() {
+
+        var templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf template engine with Spring integration")
+    public SpringTemplateEngine templateEngine() {
+
+        var templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+
+        return templateEngine;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext)
+            throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/static/**").addResourceLocations("/js/");
+    }
 
 //    @Bean
 //    public SpringTemplateEngine templateEngine() {
@@ -32,21 +99,36 @@ public class Config {
     }
 
     @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
-        return templateEngine;
+    @Description("Thymeleaf view resolver")
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        // NOTE 'order' and 'viewNames' are optional
+        viewResolver.setOrder(1);
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setViewNames(new String[]{".html", ".xhtml"});
+        return viewResolver;
     }
-    
+
     @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver() {
-        SpringResourceTemplateResolver templateResolver 
-          = new SpringResourceTemplateResolver();
+        SpringResourceTemplateResolver templateResolver
+                = new SpringResourceTemplateResolver();
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode("HTML5");
         return templateResolver;
     }
+
+    @Bean
+    @Scope("prototype")
+    public ThymeleafView mainView() {
+        ThymeleafView view = new ThymeleafView("main"); // templateName = 'main'
+        view.setStaticVariables(
+                Collections.singletonMap("footer", "The ACME Fruit Company"));
+        return view;
+    }
+
 //    @Bean(destroyMethod = "")
 //@ConditionalOnMissingBean(CamelContext.class)
 //CamelContext camelContext(ApplicationContext applicationContext,
--- src/main/java/com/dirtangel/miodigy/controller/MainController.java
+++ src/main/java/com/dirtangel/miodigy/controller/MainController.java
@@ -4,11 +4,10 @@
  */
 package com.dirtangel.miodigy.controller;
 
-
-
+import java.awt.Image;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 
@@ -22,14 +21,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class MainController {
 
-    @GetMapping("/")
-    public String showHomePage(Model model) {
+//    @GetMapping("/")
+//    public String showHomePage(Model model) {
+//        model.addAttribute("pageName", "index");
+//        model.addAttribute("appName", "miodigy");
+//        return "index";
+//    }
+
+    @RequestMapping(value = "/")
+    public String index(Model model, Pageable pageable) {
+//        final Page<Image> page = imageService.findPage(pageable);
         model.addAttribute("pageName", "index");
         model.addAttribute("appName", "miodigy");
-        return "index";
+        return "index";//return to template index.html
     }
-    
-     @GetMapping("/bio.html")
+
+    @GetMapping("/bio.html")
     public String showBio(Model model) {
         model.addAttribute("pageName", "bio");
         model.addAttribute("appName", "miodigy");
